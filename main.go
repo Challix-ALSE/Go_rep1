a701a0f4a3d3ecbc26c4769d894ec8132f10ddbe
Challix-ALSE <challix@mail.ru>
2024-09-11 22:28:54 +0300

    "Just coment main.go"

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	//Простейший вывод на консоль. Println - это вывод аргумента + `\n` (новая строка)
 	fmt.Println("Hello world")
+	fmt.Println("Second line")
+	//Функция Print - это простой вывод аргумента
+	fmt.Print("First")
+	fmt.Print("Second")
+	fmt.Print("Third")
+	// Форматированный вывод: Printf - стандартный вывод os.Stdout  c флагами форматирования
+	fmt.Printf("\nHello, my name is %s\nMy age is %d\n", "Bob", 42)
 }
