35f2b87c84b86a58d29a082a2508d49ad9a84d47
Challix-ALSE <challix@mail.ru>
2024-09-19 20:46:01 +0300

    Just comment

--- main.go
+++ main.go
@@ -14,4 +14,28 @@ func main() {
 	fmt.Printf("\nHello, my name is %s\nMy age is %d\n", "Bob", 42)
 	//////////////////////
 	//////////////////////
+	var age int
+	fmt.Println("My age is:", age)
+	age = 32
+	fmt.Println("Age after initialisation:", age)
+
+
+	// Деклврироывние и инициализация пользовательским значением
+	var height int = 183
+	fmt.Println("My height is:", height)
+
+	//В чем полустрогость типизации
+	var uid = 12345
+	fmt.Println("My uid:", uid)
+	//Декларирование и инициализация переменных одного типа (множественный слуяай)
+	var firstVar, secondVar int = 20, 30
+	fmt.Printf("firstVar:%d secondVar:%d\n", firstVar, secondVar)
+	// Декларирование блока переменных 
+	var(
+		personName string = "Bob"
+		personAge int = 42
+		personUid int
+	)
+
+	fmt.Printf("Name: %s\nAge %d\nUid: %d\n", personName, personAge, personUid)
 }
